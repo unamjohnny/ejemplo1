9f1e04df26d9ba20a7794b4c60a4b47031b321d6
Juan Carlos Carranza Moreno <accigamer77@hotmail.com>
2017-05-18 01:15:54 -0500

    Segundo commit

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use Symfony\Component\HtttpFoundation\Response;
+
+$api_key=getenv('OPEN_WEATHER_API');
+
 $app = new Silex\Application();
 
-$app->get('/clima', function() use($app) {
+$app->get('/clima', function() use($app, $api_key) {
 
 //
+      $client = new Client();
+      $url = "http://api.openweathermap.org/data/2.5/weather?id=3529947&appid=$api_key&units=metric";
+      $response = $client->get($url);
+      $body = $response->getBody();
 
-$arreglo = ["Juan_Carlos_Carranza_Moreno" => "715074876"];
+      return $body;
+      
 
-    return $app -> json($arreglo);
+
+
 });
 
 $app->run();
